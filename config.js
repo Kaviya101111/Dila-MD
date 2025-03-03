5ddb238605fec8cded68cba564ed1aaae6dfc02c
Kaviya101111 <Newnumber20252@gmail.com>
2025-03-03 16:16:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",
+MONGODB: process.env.MONGODB || "mongodb+srv://brnew8126:Ktf8ct0FluR5FvOS@cluster0.kk3r5.mongodb.net/",
 
 };
